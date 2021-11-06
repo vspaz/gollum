f83a39de11cfd1776ec6bf793c1a2367acbe8b63
vspaz <vladimir.edelman@gmail.com>
2021-11-06 20:23:43 +0100

    split into modules

---
+++ container/container.go
@@ -0,0 +1,94 @@
+package container
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func setStdInOut(cmd *exec.Cmd) *exec.Cmd {
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd
+}
+
+func fork() {
+	// execute yourself, /proc/self/exe is the copy of the binary image of the caller itself
+	cmd := exec.Command("/proc/self/exe", append([]string{"subprocess"}, os.Args[2:]...)...)
+	cmd = setStdInOut(cmd)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+
+	if err := cmd.Run(); err != nil {
+		logger.Panicf("error: %s", err.Error())
+	}
+}
+
+func NewContainer(args []string) *Container {
+	return &Container{cmd: exec.Command(args[2], args[3:]...)}
+}
+
+type Container struct {
+	cmd *exec.Cmd
+}
+
+func (c *Container) setStdStreams() {
+	c.cmd.Stdin = os.Stdin
+	c.cmd.Stdout = os.Stdout
+	c.cmd.Stderr = os.Stderr
+}
+
+func (c Container) setHostname(hostname string) {
+	err := syscall.Sethostname([]byte(hostname))
+	if err != nil {
+		logger.Errorf("failed to set hostname %s", err)
+	}
+}
+
+func (c Container) mountProc() {
+	err := syscall.Mount("proc", "proc", "proc", 0, "")
+	if err != nil {
+		logger.Errorf("failed to mount proc %s", err.Error())
+	}
+}
+
+func (c Container) mountFs(fsDirPath string) {
+	err := syscall.Chroot(fsDirPath)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (c Container) changeIntoDirectory(dirName string) {
+	err := syscall.Chdir(dirName)
+	if err != nil {
+		logger.Errorf("failed to chdir directory: %s", err.Error())
+	}
+}
+
+func (c *Container) runCommand() {
+	if err := c.cmd.Run(); err != nil {
+		logger.Panicf("failed to run cmd %s", err)
+	}
+}
+
+func (c Container) unmountProc() {
+	err := syscall.Unmount("proc", 0)
+	if err != nil {
+		logger.Errorf("failed to mount 'proc' %s", err.Error())
+	}
+}
+
+func subprocess() {
+	logger.Infof("Running %v\n", os.Args[2:])
+	container := NewContainer(os.Args)
+	container.setStdStreams()
+	container.setHostname("vspazzz")
+	container.mountFs("/home/vspaz/ubuntufs")
+	container.changeIntoDirectory("/")
+	container.mountProc()
+	container.runCommand()
+	container.unmountProc()
+}
--- container/logging.go
+++ container/logging.go
@@ -3,12 +3,14 @@ package container
 import "github.com/sirupsen/logrus"
 
 func ConfigureLogger(logLevel logrus.Level) *logrus.Logger {
-	logger := logrus.New()
+	log := logrus.New()
 	formatter := new(logrus.TextFormatter)
 	formatter.TimestampFormat = "2006-01-02 15:04:05.000"
 	formatter.FullTimestamp = true
-	logger.SetFormatter(formatter)
-	logger.SetReportCaller(true)
-	logger.SetLevel(logLevel)
-	return logger
+	log.SetFormatter(formatter)
+	log.SetReportCaller(true)
+	log.SetLevel(logLevel)
+	return log
 }
+
+var logger = ConfigureLogger(logrus.InfoLevel)
